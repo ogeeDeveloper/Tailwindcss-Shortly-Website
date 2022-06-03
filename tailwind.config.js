b71dc852a32695b64afc0713324e8872e00f3fae
ogeeDeveloper <omarogrant202@hotmail.com>
2022-06-03 09:16:03 -0500

    Created the navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,24 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     extend: {
-      // Custom Colors
+      colors: {
+        // Custom Colors
       cyan: 'hsl(180, 66%, 49%)',
       cyanLight: 'hsl(180, 66%, 69%)',
       darkViolet: 'hsl(257, 27%, 26%)',
       red: 'hsl(0, 87%, 67%)',
       grayishViolet: 'hsl(257, 7%, 63%)',
       veryDarkBlue: 'hsl(255, 11%, 22%)',
-      veryDarkViolet: 'hsl(260, 8%, 14%)'
+      veryDarkViolet: 'hsl(260, 8%, 14%) ',
+      },
+      FontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      spacing: {
+        // Extend the spacing from 97
+        180: '32rem'
+      },
     },
-    FontFamily: {
-      sans: ['Poppins', 'sans-serif'],
-    },
-    spacing: {
-      // Extend the spacing from 97
-      180: '32rem'
-    }
   },
   plugins: [],
 }
